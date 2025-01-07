ae0156244655e0942fcc9f4df22206d747a18793
Bernd Seidler <seidlerware@gmail.com>
2025-01-07 17:55:41 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    swcPlugins: [
+      ["@swc/plugin-noop/",{}],
+      // [
+      //   "swc-plugin-pre-paths",
+      //   {
+      //     "baseUrl": ".",
+      //     "paths": {
+      //       "app/*": ["./src/app/*"],
+      //       "config/*": ["./src/app/_config/*"],
+      //       "environment/*": ["./src/environments/*"],
+      //       "shared/*": ["./src/app/_shared/*"],
+      //       "helpers/*": ["./src/helpers/*"],
+      //       "tests/*": ["./src/tests/*"]
+      //     }
+      //   }
+      // ]
+    ],
+  },
 };
 
 export default nextConfig;
